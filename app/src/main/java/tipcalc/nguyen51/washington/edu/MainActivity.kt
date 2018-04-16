0a89dfa3674d55a3cfe4e97eb801e3f441cc4c55
Student User <studentuser@Students-MacBook-Pro.local>
2018-04-15 22:36:34 -0700

    Initialized android assignment

---
+++ app/src/main/java/tipcalc/nguyen51/washington/edu/MainActivity.kt
@@ -0,0 +1,12 @@
+package tipcalc.nguyen51.washington.edu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
